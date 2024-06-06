8616837b63340000d9ba925f3b8ca5a195b58e8f
jrj2004 <114087114+jrj2004@users.noreply.github.com>
2024-06-06 21:36:47 +0400

    updates in my code

--- app/src/main/java/com/example/securelinkcheckapp/LaunchAppSecureLinkCheck.kt
+++ app/src/main/java/com/example/securelinkcheckapp/LaunchAppSecureLinkCheck.kt
@@ -2,16 +2,16 @@ package com.example.securelinkcheckapp
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class LaunchAppSecureLinkCheck : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch_app_secure_link_check)
 
-        val scanButton: Button = findViewById(R.id.ScanButton)
-        scanButton.setOnClickListener {
+        val startScanButton = findViewById<Button>(R.id.ScanButton)
+        startScanButton.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
