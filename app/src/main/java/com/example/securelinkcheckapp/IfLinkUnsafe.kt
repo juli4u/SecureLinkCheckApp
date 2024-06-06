d1dc50f283f0b52c4cd109980b09700330f72aac
jrj2004 <114087114+jrj2004@users.noreply.github.com>
2024-06-06 23:10:21 +0400

    added new files and updated code

---
+++ app/src/main/java/com/example/securelinkcheckapp/IfLinkUnsafe.kt
@@ -0,0 +1,11 @@
+package com.example.securelinkcheckapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IfLinkUnsafe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_if_link_unsafe)
+    }
+}
